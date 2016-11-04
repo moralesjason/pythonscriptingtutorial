7ddb9bd5ee2eba102bd728a9f06adaea18560597
Jason Morales <jasonmorales@deltek.com>
2016-11-04 19:17:43 +0800

    More exercise 4th commit

---
+++ dictionary.py
@@ -0,0 +1,24 @@
+#!/usr/bin//env python
+import urllib
+
+properties = {}
+
+properties['protocol'] = 'http'
+properties['host'] = 'www.google.com'
+properties['port'] = '80'
+properties['path'] = '/trends/'
+
+#the properties in this map represent the URL:
+#http://www.google.com:80/trends/
+
+url = properties['protocol'] + '://' + \
+	properties['host'] + ':' + \
+	properties['port'] + \
+	properties['path']
+
+print 'Reading URL', url
+
+response = urllib.urlopen(url)
+
+print response.read()
+
---
+++ string.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+strUsers = 'rpulley  ,    jsmith, svai,  jsatriani    ,ymalmsteen    '
+
+arrUsers = strUsers.split(',')
+
+for user in arrUsers:
+	trimUser = user.strip()
+	trimUserR = user.rstrip()
+	trimUserL = user.lstrip()
+
+	firstInitial = trimUser[:1]
+	lastInitial = trimUser[1:2]
+	lastName = trimUser[1:]
+
+	print 'User : \'' + user +'\''
+	print 'LTrim: \'' + trimUserL + '\''
+	print 'RTrim: \'' + trimUserR + '\''
+	print 'Trim : \'' + trimUser + '\''
+	
+	print 'First Initial: ', firstInitial.upper()
+	print 'Last Initial : ', lastInitial.upper()
+	print 'Last Name    : ', lastName
+
+	print ''
+ 
